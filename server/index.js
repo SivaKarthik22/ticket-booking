6a598abb167a9d40f8aad6bfbd232d0ca0bd04c1
SivaKarthik22 <sivakarthik.s.2001@gmail.com>
2025-03-13 10:18:13 +0530

    error correction

--- server/index.js
+++ server/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const dbConfig = require('./config/dbConfig');
 const userRouter = require('./routes/UserRoutes');
-app.use(express.json);
+app.use(express.json());
 app.use('/api/users', userRouter);
 
-
-
-app.listen(8082, ()=>{
-    console.log("server started at port 8082");
+app.listen(8080, ()=>{
+    console.log("server started at port 8080");
 });
