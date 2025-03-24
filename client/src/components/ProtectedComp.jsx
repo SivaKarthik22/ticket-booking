5d48982b11c1b1c663f43923268ac75e86de9616
SivaKarthik22 <sivakarthik.s.2001@gmail.com>
2025-03-24 23:25:25 +0530

    protected comp ui start

--- client/src/components/ProtectedComp.jsx
+++ client/src/components/ProtectedComp.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authorizeUser } from "../user service/users";
 import { useDispatch, useSelector } from "react-redux";
-import UserSlice from "../redux/userSlice";
+import UserSlice from "../redux/UserSlice";
+import { Layout, Menu } from "antd";
 
 function ProtectedComp({children}){
     const navigate = useNavigate();
@@ -17,23 +18,32 @@ function ProtectedComp({children}){
                 dispatch(setUser(responseData.data));
             }
             else{
+                //dispatch(setUser(null));
                 navigate('/login');
             }
         }
         validateToken();
-    },[user]);
+    },[]);
 
     function logout(){
         localStorage.removeItem('token');
+        dispatch(setUser(null));
         navigate('/login');
     }
     
     return(
         <>
-            {children}
-            {user && user.name}
-            <br/>
-            <button onClick={logout} > Logout </button>
+            <Layout>
+                <Layout.Header style={{display:'flex', alignItems:'center'}}>
+                    <h3>My Day My Show</h3>
+                    <Menu mode="horizontal"></Menu>
+                    <button onClick={logout} > Logout </button>
+                </Layout.Header>
+                <Layout.Content>
+                    {children}
+                    {user && user.name}
+                </Layout.Content>
+            </Layout>
         </>
     );
 }
