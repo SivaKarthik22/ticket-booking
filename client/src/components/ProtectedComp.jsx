cd8d41fddf1d17ae8fe64cc60f53d9f238c5d0a2
SivaKarthik22 <sivakarthik.s.2001@gmail.com>
2025-03-25 09:17:59 +0530

    ui wip

--- client/src/components/ProtectedComp.jsx
+++ client/src/components/ProtectedComp.jsx
@@ -4,6 +4,7 @@ import { authorizeUser } from "../user service/users";
 import { useDispatch, useSelector } from "react-redux";
 import UserSlice from "../redux/UserSlice";
 import { Layout, Menu } from "antd";
+import SmileTwoTone from '@ant-design/icons';
 
 function ProtectedComp({children}){
     const navigate = useNavigate();
@@ -30,14 +31,43 @@ function ProtectedComp({children}){
         dispatch(setUser(null));
         navigate('/login');
     }
+
+    const navItems = [
+        {label: "Home",},
+    ];
+
+    const profileItems = [
+        {
+            label: `Hi ${user ? user.name : "guest"}`,
+            icon: <SmileTwoTone twoToneColor="#f84464" />,
+            children: [
+                {label: "Edit Profile"},
+                {label: (
+                    <span onClick={logout}>
+                        Sign out
+                    </span>
+                )},
+            ],
+        },
+    ];
     
     return(
         <>
             <Layout>
                 <Layout.Header style={{display:'flex', alignItems:'center'}}>
                     <h3>My Day My Show</h3>
-                    <Menu mode="horizontal"></Menu>
-                    <button onClick={logout} > Logout </button>
+                    <Menu 
+                        mode="horizontal"
+                        theme="dark"
+                        items={navItems}
+                        style={{ flex: 1, minWidth: 0 }}
+                    ></Menu>
+                    <Menu 
+                        mode="horizontal"
+                        theme="dark"
+                        items={profileItems}
+                        style={{ flex: 1, minWidth: 0 }}
+                    ></Menu>
                 </Layout.Header>
                 <Layout.Content>
                     {children}
