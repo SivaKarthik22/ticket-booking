55e1698214a14cbd5d8190960a0091ca72ad795f
SivaKarthik22 <sivakarthik.s.2001@gmail.com>
2025-05-06 07:53:35 +0530

    ratelimit, mongosanitize and dotenv config

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); //loads env variables into process.env
+
 const express = require('express');
 const connectDB = require('./config/dbConfig');
 const userRouter = require('./routes/UserRoutes');
@@ -6,6 +8,8 @@ const theatreRouter = require('./routes/TheatreRoutes');
 const showRouter = require('./routes/ShowRoutes');
 const bookingRouter = require('./routes/BookingRoutes');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const app = express();
 
@@ -13,9 +17,15 @@ connectDB();
 
 const clientBuildPath = path.join(__dirname, "../client/dist"); // ticket booking/client/dist
 app.use(express.static(clientBuildPath));
-/* app.get('*', (req, resp)=>{
-    resp.sendFile(path.join(clientBuildPath, "index.html"));
-}); */
+
+const limiter = rateLimit({
+    windowMs: 15*60*1000, //15 mins
+    max: 100, //each IP can make upto 100 requests
+    message: "Too many requests received from this IP. Please try again after some time",
+});
+app.use('/api/', limiter);
+
+app.use(mongoSanitize()); //saves from sql injection attacks
 
 app.use(express.json());
 app.use('/api/users', userRouter);
