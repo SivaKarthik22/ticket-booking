0135a1502c594e185592e2dee34dbe9ca99ca742
SivaKarthik22 <sivakarthik.s.2001@gmail.com>
2025-05-08 09:20:15 +0530

    lazy loading in admin page

--- client/src/components/Admin/AdminPage.jsx
+++ client/src/components/Admin/AdminPage.jsx
@@ -1,9 +1,14 @@
 import { Tabs, message } from "antd";
 import { useSelector } from "react-redux";
+import { lazy, Suspense} from 'react';
 import AccessDeny from "../AccessDeny";
-import MovieTable from "./MovieTable";
 import LoadingComp from "../LoadingComp";
-import TheatreTable from "../TheatreTable";
+
+//import TheatreTable from "../TheatreTable";
+//import MovieTable from "./MovieTable";
+
+const MovieTable = lazy(()=> import("./MovieTable"));
+const TheatreTable = lazy(()=> import("../TheatreTable"));
 
 function AdminPage(){
     const {user, userLoading} = useSelector(store=> store.user);
@@ -13,12 +18,18 @@ function AdminPage(){
         {
             key: 1,
             label: 'Movies List',
-            children: <MovieTable messageApi={messageApi} />,
+            children: 
+                <Suspense fallback={<LoadingComp/>}>
+                    <MovieTable messageApi={messageApi} />    
+                </Suspense>,
         },
         {
             key: 2,
             label: 'Theatres List',
-            children: <TheatreTable messageApi={messageApi} />,
+            children:
+                <Suspense fallback={<LoadingComp/>}>
+                    <TheatreTable messageApi={messageApi} />
+                </Suspense>,
         },
     ];
 
